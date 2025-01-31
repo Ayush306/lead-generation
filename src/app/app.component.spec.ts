472ecf693177d7d7034f84af59a926db24de3b82
ayush.tomar <ayush.tomar@worldfashionexchange.com>
2025-01-31 11:19:27 +0530

    2nd

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'lead-generation'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('lead-generation');
+    expect(app.title).toEqual('lead-generation test git');
   });
 
   // it('should render title', () => {
